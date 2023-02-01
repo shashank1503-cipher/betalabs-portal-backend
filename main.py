7f5146171907e1a6ac674f5b166b297b411cad65
Shashank Srivastava <shashank.srivastava25sks@gmail.com>
2023-02-02 00:59:39 +0530

    Added URL

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000"
+    "https://betalabs-portal-backend-production.up.railway.app/",
+    "https://betalabs-portal-backend-production.up.railway.app",
 ]
 
 app.add_middleware(
